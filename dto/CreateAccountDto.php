c39001eaee4ee03ad290aec144534529432cd804
gromitsoft <77480921+gromitsoft@users.noreply.github.com>
2021-09-05 16:26:40 +0300

    Update CreateAccountDto.php

--- dto/CreateAccountDto.php
+++ dto/CreateAccountDto.php
@@ -5,14 +5,49 @@ use GromIT\Instagram\Classes\Dto;
 class CreateAccountDto extends Dto
 {
 
-    public int $instagram_id;
-    public string $username;
-    public ?string $full_name;
-    public ?string $external_url;
-    public int $follows_count;
-    public int $followed_by_count;
-    public int $media_count;
-    public string $rapid_api_key;
-    public ?string $avatar;
+    /**
+     * @var int $instagram_id
+     */
+    public $instagram_id;
+
+    /**
+     * @var string $username
+     */
+    public $username;
+
+    /**
+     * @var string $full_name
+     */
+    public $full_name = null;
+
+    /**
+     * @var string $external_url
+     */
+    public $external_url = null;
+
+    /**
+     * @var int $follows_count
+     */
+    public $follows_count;
+
+    /**
+     * @var int $followed_by_count
+     */
+    public $followed_by_count;
+
+    /**
+     * @var int $media_count
+     */
+    public $media_count;
+
+    /**
+     * @var string $rapid_api_key
+     */
+    public $rapid_api_key;
+
+    /**
+     * @var string $avatar
+     */
+    public $avatar = null;
 
 }
