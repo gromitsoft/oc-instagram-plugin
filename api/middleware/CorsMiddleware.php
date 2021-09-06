0c091ffbd07e3d288d5095f644cf5b71fd0c8243
slipgrom <gfhjkm85>
2021-09-06 09:39:29 +0300

    fix

--- api/middleware/CorsMiddleware.php
+++ api/middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'zalupa.gromit.host')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', '*');
     }
